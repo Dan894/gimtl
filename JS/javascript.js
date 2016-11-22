d0d10a309aa65006103a2b13d991a9591db60148
Dan894 <daniel.jaffke@beuth-hochschule.de>
2016-11-22 15:45:43 +0100

    Tuesday effective
    mit Flori erneuert und angepasst
    Grundlayout erstellt und ersten Abhängigen Daten

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', '3rdPage'],
-    sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+    //sectionsColor: ['#fff', '#fff', '#7E8F7C', '#7E8F7C'],
+    menu: '#menu',
     css3: true
+  });
+  // var elem = document.getElementsByClas("section");
+  // elem.addEventListener("change", Auswählen);
+  //
+  // function Auswählen() {
+  //   var head = document.getElementById("header");
+  //   console.log(head.innerHTML);
+  //   var x = document.getElementsByClass("section").value;
+  //   alert('Sie haben einen '+x+' ausgewählt!');
+  // }
+  // $('.section').onChange(function(){
+  //
+  // });
+  console.log(document.getElementById('fullpage'));
+  $("#fullpage").change(function(){
+    alert("The text has been changed.");
   });
 });
