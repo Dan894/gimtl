f4dca4a882f57670c58e70c342910939c0b8ae3d
Dan894 <daniel.jaffke@beuth-hochschule.de>
2016-11-28 15:56:03 +0100

    Bilder angelegt mit Photoshop und Responsive Darstellung
    Bilder zu den Personen
    Legende der Abteilungen
    Absprache mit Dozent
    Responsive Design erzeugt mit Tests in den Versionen

--- JS/javascript.js
+++ JS/javascript.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
     //sectionsColor: ['#fff', '#fff', '#7E8F7C', '#7E8F7C'],
     menu: '#menu',
+    // scrollBar: true,
+    // autoScrolling: false;
+    // normalScrollElements: '#gruppenbild',
+    scrollOverflow: false,
     css3: true
   });
   // var elem = document.getElementsByClas("section");
