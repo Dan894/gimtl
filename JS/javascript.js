7b0d6572dd821dd9eb4f8cacf980d9715dbc0f08
Dan894 <daniel.jaffke@beuth-hochschule.de>
2017-01-10 15:40:35 +0100

    10.01.2017
    Verarbeitung

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
-    //sectionsColor: ['#fff', '#fff', '#7E8F7C', '#7E8F7C'],
     menu: '#menu',
-    // scrollBar: true,
-    // autoScrolling: false;
-    // normalScrollElements: '#gruppenbild',
     scrollOverflow: false,
     css3: true
   });
-  $(function () {
-    $('[data-toggle="popover"]').popover();
+  // $(function () {
+  //   $('[data-toggle="popover"]').popover();
+  // });
+  $('#dropdown-menu-mobile').click(function (){
+    $('#mobile-dropdown').toggleClass('active');
   });
-  
 });
